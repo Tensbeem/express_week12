64c7eb29b2db673ffd65311d2734efe0073848c8
Tensbeam <tensrrt123@gmail.com>
2022-08-15 14:20:28 +0700

    update

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const http = require('http')
+const user = require('./routes/user')
+
+const app = express()
+
+app.use('/v1',user)
+
+const server = http.createServer(app)
+
+server.listen(5000,(err)=>{
+    if(err)
+        console.log(err)
+        console.log(`Server running at http://localhost:5000`)
+    
+})
